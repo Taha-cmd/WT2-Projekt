5cf3d2439c55958ca1ffcb80708e83223b8940ce
Taha <taharaed98@gmail.com>
2020-05-07 09:21:29 +0200

    test pull request

--- layout/js/layout.js
+++ layout/js/layout.js
@@ -1,29 +1,25 @@
 /* drop down menu -> header*/
-$('#burger-icon').click(function(){
-    $('#navbar').slideToggle();
+$("#burger-icon").click(function () {
+	$("#navbar").slideToggle();
 });
 
-$(window).on('resize', function(){
-    if($(window).width() > 768){
-        $('#navbar').show();
-    } else if($(window).width() <= 768){
-        $('#navbar').hide();
-    }
+$(window).on("resize", function () {
+	if ($(window).width() > 768) {
+		$("#navbar").show();
+	} else if ($(window).width() <= 768) {
+		$("#navbar").hide();
+	}
 });
-
 
 /* login modal -> login*/
-$('#login-link').click(function(){
-    $('#modal').slideDown();
-    $('#modal-content').fadeIn(500);
+$("#login-link").click(function () {
+	$("#modal").slideDown();
+	$("#modal-content").fadeIn(500);
 });
 
-$('#modal').click(function(event){
-    if(event.target == this){
-        $(this).slideUp();
-        $('#modal-content').fadeOut(500);
-    }
+$("#modal").click(function (event) {
+	if (event.target == this) {
+		$(this).slideUp();
+		$("#modal-content").fadeOut(500);
+	}
 });
-
-
-
